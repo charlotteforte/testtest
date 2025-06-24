653d53a5fade1dd3d80b7da6d8daac1405018e2b
Hello Leo <hello@terros.io>
2025-06-24 14:22:06 +0000

    helloleo: feat: add image carousel and modernize services section with webyansh.com inspired design

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import Services from '@/components/Services'
 import About from '@/components/About'
+import ImageCarousel from '@/components/ImageCarousel'
 import Portfolio from '@/components/Portfolio'
 import Contact from '@/components/Contact'
 import Footer from '@/components/Footer'
@@ -11,8 +12,9 @@ function App() {
     <div className="min-h-screen bg-white">
       <Header />
       <Hero />
-      <Services />
       <About />
+      <ImageCarousel />
+      <Services />
       <Portfolio />
       <Contact />
       <Footer />
